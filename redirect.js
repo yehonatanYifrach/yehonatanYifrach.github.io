33f03b565039f0d95cfcbae9105bf557a497d0b4
yehonatanYifrach <56507675+yehonatanYifrach@users.noreply.github.com>
2024-09-16 17:55:15 +0300

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,22 @@
+function parse_phonenumber(phone){
+    if(phone.startsWith("05")){
+        return "+972"+phone.slice(1)
+    }
+    return phone
+
+
+}
+
+
+async function redirect()  {
+    console.log("you will be redirected...")
+    let text = "שלום תורנים יקרים!"
+    let await_json = await fetch("toranim.json")
+    let json_phones = await await_json.text()
+    json_phones = JSON.parse(json_phones)
+    let phone = [json_phones["A"],json_phones["B"]].map((x)=>x["phone"])[Math.floor(Math.random() * 2)]
+    var link_format = `https://api.whatsapp.com/send/?phone=${encodeURI(parse_phonenumber(phone))}&text=${encodeURI(text)}`
+    window.location.replace(link_format)
+}
+
+redirect()
